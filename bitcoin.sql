0c50158311b5fc031c2fdfdd01314c71b51ecbe1
Attauyo <136933744+Attauyo@users.noreply.github.com>
2024-08-26 23:00:50 +0100

    Update bitcoin.sql

--- bitcoin.sql
+++ bitcoin.sql
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS admins (
 );
 -- Create the transactions table
 CREATE TABLE transactions (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT
     type ENUM('deposit', 'withdrawal', 'investment') NOT NULL,
     amount DECIMAL(15, 2) NOT NULL,
     date DATETIME DEFAULT CURRENT_TIMESTAMP,
