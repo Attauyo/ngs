e9a268c4df5a5e8deb7e129b6985d7b7f6bdff08
Attauyo <136933744+Attauyo@users.noreply.github.com>
2024-08-26 22:36:39 +0100

    Update bitcoin.sql

--- bitcoin.sql
+++ bitcoin.sql
@@ -1,7 +1,6 @@
 -- Users table
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     balance DECIMAL(15, 2) DEFAULT 0.00
@@ -15,8 +14,7 @@ CREATE TABLE IF NOT EXISTS admins (
 );
 -- Create the transactions table
 CREATE TABLE transactions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     type ENUM('deposit', 'withdrawal', 'investment') NOT NULL,
     amount DECIMAL(15, 2) NOT NULL,
     date DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -44,16 +42,16 @@ CREATE TABLE IF NOT EXISTS notifications (
 );
 
 -- Insert example users
-INSERT INTO users (username, password, balance) VALUES
+INSERT INTO users (user_id, username, password, balance) VALUES
 (1,'charity', 'charity', 1000.00),
 (2,'user2', 'password456', 2000.00);
 
 -- Insert example admins
-INSERT INTO admins (username, password) VALUES
+INSERT INTO admins (admin_id, username, password) VALUES
 (1, 'admin', 'admin'),
 
 -- Insert example notifications
-INSERT INTO notifications (message, type, recipient, recipient_username, date) VALUES
+INSERT INTO notifications (notification_id, message, type, recipient, recipient_username, date) VALUES
 (1, 'Investment successful!', 'success', 'user', 'user1', NOW()),
 (2, 'Your balance is low.', 'warning', 'user', 'user2', NOW()),
 (3, 'Admin login attempt.', 'info', 'admin', 'admin1', NOW());
